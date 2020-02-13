0d74e6d5682a1089dc90ae58b6a38775acf348dc
Joshua A Jack <mynameisjoshuajack@gmail.com>
2020-02-13 00:33:51 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,13 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 board = [" "," "," "," "," "," "," "," "," "]
-
-def display_board(board)
+board_1 = [" "," "," "," "," x "," "," "," "," "]
+board = [" "," "," "," "," "," "," "," "," "]
+board = [" "," "," "," "," "," "," "," "," "]
+board = [" "," "," "," "," "," "," "," "," "]
+board = [" "," "," "," "," "," "," "," "," "]
+board = [" "," "," "," "," "," "," "," "," "]
+def display_board(board,board_1)
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
