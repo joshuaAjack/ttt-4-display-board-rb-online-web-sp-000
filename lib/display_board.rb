125cc9c126e1f8b35d23b9fbeeda2de2f76db73e
Joshua A Jack <mynameisjoshuajack@gmail.com>
2020-02-13 00:49:29 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,19 +3,19 @@
 board = [" "," "," "," "," "," "," "," "," "]
 def display_board(board)
   puts "   |   |   " == rows[0]
-  puts "-----------" == rows[2]
-  puts "   |   |   " == rows[3]
-  puts "-----------" == rows[4]
-  puts "   |   |   " == rows[5]
+  puts "-----------" == rows[1]
+  puts "   |   |   " == rows[2]
+  puts "-----------" == rows[3]
+  puts "   |   |   " == rows[4]
 
  end
 
  center_x = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
  def display_board(center_x)
   puts "   |   |   " == rows[0]
-  puts "-----------" == rows[2]
-  puts "   | X |   " == rows[3]
-  puts "-----------" == rows[4]
-  puts "   |   |   " == rows[5]
+  puts "-----------" == rows[1]
+  puts "   | X |   " == rows[2]
+  puts "-----------" == rows[3]
+  puts "   |   |   " == rows[4]
   
  end
