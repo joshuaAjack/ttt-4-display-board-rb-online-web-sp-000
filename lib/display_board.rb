0a4e98d2989a555739c57ba98889628739f5e72e
Joshua A Jack <mynameisjoshuajack@gmail.com>
2020-02-13 00:38:11 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -9,3 +9,12 @@ def display_board(board)
   puts "   |   |   " == rows[5]
 
  end
+ 
+ def display_board(board)
+  puts "   |   |   " == rows[0]
+  puts "-----------" == rows[2]
+  puts "   |  |   " == rows[3]
+  puts "-----------" == rows[4]
+  puts "   |   |   " == rows[5]
+
+ end
