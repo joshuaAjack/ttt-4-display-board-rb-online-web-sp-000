932bcff4f94d4164f4d0c4d729bc30e216f91ecc
Joshua A Jack <mynameisjoshuajack@gmail.com>
2020-02-13 00:40:08 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -10,11 +10,13 @@ def display_board(board)
 
  end
  
+ board = [" "," "," "," "," x"," "," "," "," "]
  def display_board(board)
- expect(rows[0]).to eq("   |   |   ")
-      expect(rows[1]).to eq("-----------")
-      expect(rows[2]).to eq("   |   |   ")
-      expect(rows[3]).to eq("-----------")
-      expect(rows[4]).to eq("   |   |   ")
+  puts "   |   |   " == rows[0]
+  puts "-----------" == rows[2]
+  puts "   |  x |   " == rows[3]
+  puts "-----------" == rows[4]
+  puts "   |   |   " == rows[5]
+ 
 
  end
